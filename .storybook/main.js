bc69b52facf49aecfb88f8fa29c21f9df164185b
Cory Reed <swashcap@gmail.com>
2021-06-14 22:21:56 -0700

    chore: upgrade @storybook/* to 6.2.9

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,14 @@ module.exports = {
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   /**
+   * Disable react-docgen until this is fixed:
+   * {@link https://github.com/styleguidist/react-docgen-typescript/issues/366}
+   * Related: {@link https://github.com/storybookjs/storybook/issues/15067}
+   */
+  typescript: {
+    reactDocgen: false,
+  },
+  /**
    * Customize Webpack config.
    * {@link https://storybook.js.org/docs/configurations/custom-webpack-config/}
    */
